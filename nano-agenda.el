6c38e95b8e846aceb88398c682fd283052924556
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2023-04-10 13:11:49 +0200

    Added choice for calendar busy colors

--- nano-agenda.el
+++ nano-agenda.el
@@ -4,7 +4,7 @@
 
 ;; Maintainer: Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
 ;; URL: https://github.com/rougier/nano-agenda
-;; Version: 0.2.2
+;; Version: 0.3
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: convenience, org-mode, org-agenda
 
@@ -37,6 +37,10 @@
 ;; M-x: nano-agenda
 ;;
 ;;; NEWS:
+;;
+;; Version 0.3.0
+;; - Use of a single buffer for calendar + agenda
+;; - Added palette choice for colorizing the calendar
 ;;
 ;; Version 0.2.2
 ;; - Better entries sorting
@@ -93,18 +97,65 @@ This function takes an entry and the selected date. Returns a value if the entry
 should be shown, otherwise, returns nil."
   :group 'nano-agenda)
 
-(defcustom nano-agenda-busy-backgrounds  (list "#FFF9DB" "#FFF3BF" "#FFEC99" "#FFE066" "#FFD43B"
-                                               "#FCC419" "#FAB005" "#F59F00" "#F08C00" "#E67700")
-  "Background colors to be used to highlight a day in calendar
-  view according to busy level."
-  :type '(repeat color)
-  :group 'nano-agenda-faces)
+;; See https://material.io/design/color/the-color-system.html
+(defvar nano-agenda-palettes
+  '((red         . ("#FFEBEE" "#FFCDD2" "#EF9A9A" "#E57373" "#EF5350"
+                    "#F44336" "#E53935" "#D32F2F" "#C62828" "#B71C1C"))
+    (pink        . ("#FCE4EC" "#F8BBD0" "#F48FB1" "#F06292" "#EC407A"
+                    "#E91E63" "#D81B60" "#C2185B" "#AD1457" "#880E4F"))
+    (purple      . ("#F3E5F5" "#E1BEE7" "#CE93D8" "#BA68C8" "#AB47BC"
+                    "#9C27B0" "#8E24AA" "#7B1FA2" "#6A1B9A" "#4A148C"))
+    (deep-purple . ("#EDE7F6" "#D1C4E9" "#B39DDB" "#9575CD" "#7E57C2"
+                    "#673AB7" "#5E35B1" "#512DA8" "#4527A0" "#311B92"))
+    (indigo      . ("#E8EAF6" "#C5CAE9" "#9FA8DA" "#7986CB" "#5C6BC0"
+                    "#3F51B5" "#3949AB" "#303F9F" "#283593" "#1A237E"))
+    (blue        . ("#E3F2FD" "#BBDEFB" "#90CAF9" "#64B5F6" "#42A5F5"
+                    "#2196F3" "#1E88E5" "#1976D2" "#1565C0" "#0D47A1"))
+    (light-blue  . ("#E1F5FE" "#B3E5FC" "#81D4FA" "#4FC3F7" "#29B6F6"
+                    "#03A9F4" "#039BE5" "#0288D1" "#0277BD" "#01579B"))
+    (cyan        . ("#E0F7FA" "#B2EBF2" "#80DEEA" "#4DD0E1" "#26C6DA"
+                    "#00BCD4" "#00ACC1" "#0097A7" "#00838F" "#006064"))
+    (teal        . ("#E0F2F1" "#B2DFDB" "#80CBC4" "#4DB6AC" "#26A69A"
+                    "#009688" "#00897B" "#00796B" "#00695C" "#004D40"))
+    (green       . ("#E8F5E9" "#C8E6C9" "#A5D6A7" "#81C784" "#66BB6A"
+                    "#4CAF50" "#43A047" "#388E3C" "#2E7D32" "#1B5E20"))
+    (light-green . ("#F1F8E9" "#DCEDC8" "#C5E1A5" "#AED581" "#9CCC65"
+                    "#8BC34A" "#7CB342" "#689F38" "#558B2F" "#33691E"))
+    (lime        . ("#F9FBE7" "#F0F4C3" "#E6EE9C" "#DCE775" "#D4E157"
+                    "#CDDC39" "#C0CA33" "#AFB42B" "#9E9D24" "#827717"))
+    (yellow      . ("#FFFDE7" "#FFF9C4" "#FFF59D" "#FFF176" "#FFEE58"
+                    "#FFEB3B" "#FDD835" "#FBC02D" "#F9A825" "#F57F17"))
+    (amber       . ("#FFF8E1" "#FFECB3" "#FFE082" "#FFD54F" "#FFCA28"
+                    "#FFC107" "#FFB300" "#FFA000" "#FF8F00" "#FF6F00"))
+    (orange      . ("#FFF3E0" "#FFE0B2" "#FFCC80" "#FFB74D" "#FFA726"
+                    "#FF9800" "#FB8C00" "#F57C00" "#EF6C00" "#E65100"))
+    (deep-orange . ("#FBE9E7" "#FFCCBC" "#FFAB91" "#FF8A65" "#FF7043"
+                    "#FF5722" "#F4511E" "#E64A19" "#D84315" "#BF360C"))
+    (brown       . ("#EFEBE9" "#D7CCC8" "#BCAAA4" "#A1887F" "#8D6E63"
+                    "#795548" "#6D4C41" "#5D4037" "#4E342E" "#3E2723"))
+    (grey        . ("#FAFAFA" "#F5F5F5" "#EEEEEE" "#E0E0E0" "#BDBDBD"
+                    "#9E9E9E" "#757575" "#616161" "#424242" "#212121"))
+    (blue-grey   . ("#ECEFF1" "#CFD8DC" "#B0BEC5" "#90A4AE" "#78909C"
+                    "#607D8B" "#546E7A" "#455A64" "#37474F" "#263238"))))
 
-(defcustom nano-agenda-busy-foregrounds (list "#000000" "#000000" "#000000" "#000000" "#000000"
-                                              "#000000" "#000000" "#000000" "#000000" "#FFFFFF")
-  "Foreground colors to be used to highlight a day in calendar
+
+(defun nano-agenda-color-luminance (color)
+  "Calculate the relative luminance of a color string (e.g. \"#ffaa00\", \"blue\").
+Return a value between 0 and 1."
+  (let* ((values (x-color-values color))
+         (red (/ (car values) 256.0))
+         (green (/ (cadr values) 256.0))
+         (blue (/ (caddr values) 256.0)))
+    (/ (+ (* .2126 red) (* .7152 green) (* .0722 blue)) 255)))
+
+(defcustom nano-agenda-palette 'blue-grey
+  "Background colors to use to highlight a day in calendar
   view according to busy level."
-  :type '(repeat color)
+  :type `(choice (const red)    (const pink)  (const purple)      (const deep-purple)
+                 (const indigo) (const blue)  (const light-blue)  (const cyan)
+                 (const teal)   (const green) (const light-green) (const lime)
+                 (const yellow) (const amber) (const orange)      (const deep-orange)
+                 (const brown)  (const grey)  (const blue-grey))
   :group 'nano-agenda-faces)
 
 (defface nano-agenda-default
@@ -570,10 +621,10 @@ for efficiency."
                ;; Slow
                (level (nano-agenda--busy-level date))
                (level (min (length nano-agenda-busy-foregrounds) level))
-               (foreground (nth (- level 1) nano-agenda-busy-foregrounds))
-               (background (nth (- level 1) nano-agenda-busy-backgrounds))
-               ;; ----
-
+               (backgrounds (alist-get nano-agenda-palette nano-agenda-palettes))
+               (background (nth (- level 1) backgrounds))
+               (foreground (if (< (nano-agenda-color-luminance background) 0.5)
+                               "white" "black"))
                (map (make-sparse-keymap))
                (is-today (nano-agenda-date-is-today date))
                (has-deadline (nano-agenda--has-deadline date))
